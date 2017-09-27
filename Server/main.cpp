f44f6f25671831ae5b6a14e35b132f718c247599
vaspull <vaspull@mail.ru>
2017-09-27 17:14:49 +0300

    1

--- Server/main.cpp
+++ Server/main.cpp
@@ -56,7 +56,7 @@ std::string sha512(std::string input);
     *((str) + 2) = (uint8) ((x) >>  8);       \
     *((str) + 1) = (uint8) ((x) >> 16);       \
     *((str) + 0) = (uint8) ((x) >> 24);       \
-}
+    }
 #define SHA2_UNPACK64(x, str)                 \
 {                                             \
     *((str) + 7) = (uint8) ((x)      );       \
@@ -67,61 +67,61 @@ std::string sha512(std::string input);
     *((str) + 2) = (uint8) ((x) >> 40);       \
     *((str) + 1) = (uint8) ((x) >> 48);       \
     *((str) + 0) = (uint8) ((x) >> 56);       \
-}
+    }
 #define SHA2_PACK64(str, x)                   \
 {                                             \
     *(x) =   ((uint64) *((str) + 7)      )    \
-           | ((uint64) *((str) + 6) <<  8)    \
-           | ((uint64) *((str) + 5) << 16)    \
-           | ((uint64) *((str) + 4) << 24)    \
-           | ((uint64) *((str) + 3) << 32)    \
-           | ((uint64) *((str) + 2) << 40)    \
-           | ((uint64) *((str) + 1) << 48)    \
-           | ((uint64) *((str) + 0) << 56);   \
-}
+    | ((uint64) *((str) + 6) <<  8)    \
+    | ((uint64) *((str) + 5) << 16)    \
+    | ((uint64) *((str) + 4) << 24)    \
+    | ((uint64) *((str) + 3) << 32)    \
+    | ((uint64) *((str) + 2) << 40)    \
+    | ((uint64) *((str) + 1) << 48)    \
+    | ((uint64) *((str) + 0) << 56);   \
+    }
 
 
 const unsigned long long SHA512::sha512_k[80] = //ULL = uint64
-            {0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL,
-             0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL,
-             0x3956c25bf348b538ULL, 0x59f111f1b605d019ULL,
-             0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL,
-             0xd807aa98a3030242ULL, 0x12835b0145706fbeULL,
-             0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL,
-             0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL,
-             0x9bdc06a725c71235ULL, 0xc19bf174cf692694ULL,
-             0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL,
-             0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
-             0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL,
-             0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL,
-             0x983e5152ee66dfabULL, 0xa831c66d2db43210ULL,
-             0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL,
-             0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL,
-             0x06ca6351e003826fULL, 0x142929670a0e6e70ULL,
-             0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL,
-             0x4d2c6dfc5ac42aedULL, 0x53380d139d95b3dfULL,
-             0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL,
-             0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
-             0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL,
-             0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL,
-             0xd192e819d6ef5218ULL, 0xd69906245565a910ULL,
-             0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL,
-             0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL,
-             0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL,
-             0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL,
-             0x5b9cca4f7763e373ULL, 0x682e6ff3d6b2b8a3ULL,
-             0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL,
-             0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
-             0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL,
-             0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL,
-             0xca273eceea26619cULL, 0xd186b8c721c0c207ULL,
-             0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL,
-             0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL,
-             0x113f9804bef90daeULL, 0x1b710b35131c471bULL,
-             0x28db77f523047d84ULL, 0x32caab7b40c72493ULL,
-             0x3c9ebe0a15c9bebcULL, 0x431d67c49c100d4cULL,
-             0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL,
-             0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL};
+{0x428a2f98d728ae22ULL, 0x7137449123ef65cdULL,
+        0xb5c0fbcfec4d3b2fULL, 0xe9b5dba58189dbbcULL,
+        0x3956c25bf348b538ULL, 0x59f111f1b605d019ULL,
+        0x923f82a4af194f9bULL, 0xab1c5ed5da6d8118ULL,
+        0xd807aa98a3030242ULL, 0x12835b0145706fbeULL,
+        0x243185be4ee4b28cULL, 0x550c7dc3d5ffb4e2ULL,
+        0x72be5d74f27b896fULL, 0x80deb1fe3b1696b1ULL,
+        0x9bdc06a725c71235ULL, 0xc19bf174cf692694ULL,
+        0xe49b69c19ef14ad2ULL, 0xefbe4786384f25e3ULL,
+        0x0fc19dc68b8cd5b5ULL, 0x240ca1cc77ac9c65ULL,
+        0x2de92c6f592b0275ULL, 0x4a7484aa6ea6e483ULL,
+        0x5cb0a9dcbd41fbd4ULL, 0x76f988da831153b5ULL,
+        0x983e5152ee66dfabULL, 0xa831c66d2db43210ULL,
+        0xb00327c898fb213fULL, 0xbf597fc7beef0ee4ULL,
+        0xc6e00bf33da88fc2ULL, 0xd5a79147930aa725ULL,
+        0x06ca6351e003826fULL, 0x142929670a0e6e70ULL,
+        0x27b70a8546d22ffcULL, 0x2e1b21385c26c926ULL,
+        0x4d2c6dfc5ac42aedULL, 0x53380d139d95b3dfULL,
+        0x650a73548baf63deULL, 0x766a0abb3c77b2a8ULL,
+        0x81c2c92e47edaee6ULL, 0x92722c851482353bULL,
+        0xa2bfe8a14cf10364ULL, 0xa81a664bbc423001ULL,
+        0xc24b8b70d0f89791ULL, 0xc76c51a30654be30ULL,
+        0xd192e819d6ef5218ULL, 0xd69906245565a910ULL,
+        0xf40e35855771202aULL, 0x106aa07032bbd1b8ULL,
+        0x19a4c116b8d2d0c8ULL, 0x1e376c085141ab53ULL,
+        0x2748774cdf8eeb99ULL, 0x34b0bcb5e19b48a8ULL,
+        0x391c0cb3c5c95a63ULL, 0x4ed8aa4ae3418acbULL,
+        0x5b9cca4f7763e373ULL, 0x682e6ff3d6b2b8a3ULL,
+        0x748f82ee5defb2fcULL, 0x78a5636f43172f60ULL,
+        0x84c87814a1f0ab72ULL, 0x8cc702081a6439ecULL,
+        0x90befffa23631e28ULL, 0xa4506cebde82bde9ULL,
+        0xbef9a3f7b2c67915ULL, 0xc67178f2e372532bULL,
+        0xca273eceea26619cULL, 0xd186b8c721c0c207ULL,
+        0xeada7dd6cde0eb1eULL, 0xf57d4f7fee6ed178ULL,
+        0x06f067aa72176fbaULL, 0x0a637dc5a2c898a6ULL,
+        0x113f9804bef90daeULL, 0x1b710b35131c471bULL,
+        0x28db77f523047d84ULL, 0x32caab7b40c72493ULL,
+        0x3c9ebe0a15c9bebcULL, 0x431d67c49c100d4cULL,
+        0x4cc5d4becb3e42b6ULL, 0x597f299cfc657e2aULL,
+        0x5fcb6fab3ad6faecULL, 0x6c44198c4a475817ULL};
 
 void SHA512::transform(const unsigned char *message, unsigned int block_nb)
 {
@@ -143,7 +143,7 @@ void SHA512::transform(const unsigned char *message, unsigned int block_nb)
         }
         for (j = 0; j < 80; j++) {
             t1 = wv[7] + SHA512_F2(wv[4]) + SHA2_CH(wv[4], wv[5], wv[6])
-                + sha512_k[j] + w[j];
+                    + sha512_k[j] + w[j];
             t2 = SHA512_F1(wv[0]) + SHA2_MAJ(wv[0], wv[1], wv[2]);
             wv[7] = wv[6];
             wv[6] = wv[5];
@@ -205,7 +205,7 @@ void SHA512::final(unsigned char *digest)
     unsigned int len_b;
     int i;
     block_nb = 1 + ((SHA384_512_BLOCK_SIZE - 17)
-                     < (m_len % SHA384_512_BLOCK_SIZE));
+                    < (m_len % SHA384_512_BLOCK_SIZE));
     len_b = (m_tot_len + m_len) << 3;
     pm_len = block_nb << 7;
     memset(m_block + m_len, 0, pm_len - m_len);
@@ -272,7 +272,7 @@ void reguser()
             ofstream hashWriteTemp("pwdtemp.txt", ios_base::trunc);
             while( getline(summHashChek, tempwrite) ){
                 if ( tempwrite != summHash){
-                hashWriteTemp << tempwrite << '\n'; }
+                    hashWriteTemp << tempwrite << '\n'; }
             }
             hashWriteTemp.close();
             summHashChek.close();
@@ -423,6 +423,8 @@ void SendMessageToClient(int ID)
 {
     for ( ; ; Sleep(750))
     {
+        ofstream logs("logs.txt", ios_base::app);
+        logs.close();
         char buffer[50000] = "";
         for (int clear666 = 0; buffer[clear666] != 0; clear666++) buffer[clear666] = '\0';
         if (recv(Connections[ID], buffer, sizeof(buffer), 0)){
@@ -434,7 +436,10 @@ void SendMessageToClient(int ID)
             deshifr(buffer,key);
             parce(buffer, name, pwd, text, res);
             if(valid(name,pwd) == 0){
-                printf("client id %d: <- NO VALID!!!\n", ID);
+                logs.open("logs.txt", ios_base::app);
+                logs << "client id: " << ID << "<- NO VALID!!!\n";
+                logs.close();
+                //printf("client id %d: <- NO VALID!!!\n", ID);
                 shifr(accden,key);
                 send(Connections[ID],accden, sizeof(accden), 0);
                 deshifr(accden,key);
@@ -445,9 +450,12 @@ void SendMessageToClient(int ID)
                 break;
             }
             else if(res[0] != '\0'){
-                printf("client id %d: ", ID);
-                printf("client id %d: \n", ClientCount);
-                printf("%s",res);
+                logs.open("logs.txt", ios_base::app);
+                logs << "client id: " << ID << ":" << res;
+                logs.close();
+//                printf("client id %d: ", ID);
+//                printf("client id %d: \n", ClientCount);
+//                printf("%s",res);
                 for (int i = 0; i < ClientCount; i++) //Отправка каждому подключенному клиенту
                 {
                     if (i!=ID){
@@ -461,7 +469,10 @@ void SendMessageToClient(int ID)
                 for (int clear666 = 0; res[clear666] != 0; clear666++) res[clear666] = '\0';
             }
             else {
-                printf("client id %d: <- DISCONNECT!\n", ID);
+                logs.open("logs.txt", ios_base::app);
+                logs << "client id: " << ID << " <- DISCONNECT!\n";
+                logs.close();
+                //printf("client id %d: <- DISCONNECT!\n", ID);
                 shifr(accden,key);
                 send(Connections[ID],accden, sizeof(accden), 0);
                 deshifr(accden,key);
@@ -491,6 +502,8 @@ void SendMessageToClient(int ID)
 
 int main()
 {
+    ofstream logs("logs.txt", ios_base::app);
+    logs.close();
     FILE* pwd = fopen("pwd.txt", "r");
     if (!pwd) {
         printf("File pwd.txt does not exist. Check paths.\n");
@@ -506,10 +519,8 @@ int main()
     if(res!=0)  {
         return 0;
     }
-
     struct addrinfo hints;
     struct addrinfo * result;
-
     Connections = (SOCKET*)calloc(64,sizeof(SOCKET));
     ZeroMemory(&hints,sizeof(hints));
     hints.ai_family = AF_INET;
@@ -520,16 +531,17 @@ int main()
     Listen = socket(result->ai_family, result->ai_socktype,result->ai_protocol);
     bind(Listen,result->ai_addr,result->ai_addrlen);
     listen(Listen,SOMAXCONN);
-
     freeaddrinfo(result);
-
     printf("Start server...\n");
     char m_connect[] = "online\n\n";
     shifr(m_connect,key);
     CreateThread(0,0,(LPTHREAD_START_ROUTINE)reguser,0,0,0);
     for(;;Sleep(75)) {
         if((Connect = accept(Listen,0,0)) != SOCKET_ERROR) {
-            printf("Client id:%d connect...\n", ClientCount);
+            logs.open("logs.txt", ios_base::app);
+            logs << "Client id: " << ClientCount << "connect...\n";
+            logs.close();
+//          printf("Client id:%d connect...\n", ClientCount);
             Connections[ClientCount] = Connect;
             send(Connections[ClientCount],m_connect,strlen(m_connect),0);
             ClientCount++;
