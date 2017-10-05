d78ca829241ed5ed5625f67194ae14b181041b12
vaspull <vaspull@mail.ru>
2017-10-05 15:41:09 +0300

    переписаны функции parcer, valid, crypt, decrypt, sendmessagetoclient, а также int main, по максимуму убраны строковые массивы типа char[]   где это было возможно

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,15 +1,18 @@
 #include <ws2tcpip.h>
 #include <iostream>
+#include <windows.h>
+#include <string>
 #include <fstream>
-#include <string.h>
-#include <ctype.h>
-#include <time.h>
 #include <sha512.h>
+#include <time.h>
+#include <ctype.h>
 #define maxlenghlogin 10
 #define minlenghlogin 6
 #define maxlenghpwd 12
 #define minlenghpwd 6
 #define keysize 1024
+#define buffersize 50000
+#define sleeptime 750
 
 const char key[keysize] = "key";
 
@@ -34,7 +37,7 @@ void reguser()
     printf("User registration program\n\n");
     std::ifstream read;
     std::ofstream write;
-    for(;;Sleep(50))
+    for(;;Sleep(sleeptime))
     {
         int isTruePwdLengh = 1, isTrueLoginLengh = 1, isExist = 1;
         std::string login, pwd, hash, summ, findeExistUser, isAgain, copy;
@@ -156,13 +159,13 @@ void reguser()
     }
 }
 
-void crypt (std::string &res, const std::string key)  //updated text encryption function, std :: string is used, arrays char [] are removed, superfluous break and encryption logic is improved
+void crypt (std::string &res, const std::string key)
 {
     int keydigit = 0, istruevalue = 1, isend = 1;
     std::string trash, itog;
-    for(unsigned int i = 0; i <= strlen(res.c_str())*2; i++){   //creating a garbage line that merges with the message being sent
-        trash += rand() % 127;                                  //first there are 2 symbols of garbage, then 1 character of the transmitted message,
-        for(;istruevalue;)                                      //after the last character there are also 2 garbage characters
+    for(unsigned int i = 0; i <= strlen(res.c_str())*2; i++){
+        trash += rand() % 127;
+        for(;istruevalue;)
         {
             if(isalnum(trash[i]) == 0) {
                 trash[i] = rand()%127;
@@ -187,19 +190,18 @@ void crypt (std::string &res, const std::string key)  //updated text encryption 
         }
         istruevalue = 1;
     }
-
-    for(unsigned int a = 0, j = 0; a <= strlen(res.c_str()); ){ //an auxiliary line itog, it will write garbage alternating characters of the transmitted message
+    for(unsigned int a = 0, j = 0; a <= strlen(res.c_str()); ){
         itog += trash[j++];
         itog += trash[j++];
         itog += res[a++];
     }
     res.clear();
     res = itog;
-    keydigit = strlen(res.c_str()) % 4;  //keydigit has the value of the remainder of dividing the length of the string with junk and desired characters by 4
-    for (unsigned int i = 0; i < strlen(res.c_str());)                      //in this cycle and there is an encryption, to each character code of a line with garbage and necessary symbols
-    {                                                                       //adds the key symbol code and keydigit
-        for (unsigned int a = 0; (a < strlen(key.c_str())) && isend;a++)    //the first character of the key, the second second, and so on, is added to the first character of the string.
-        {                                                                   //If the string does not end, or the key does not end, and then the key reads
+    keydigit = strlen(res.c_str()) % 4;
+    for (unsigned int i = 0; i < strlen(res.c_str());)
+    {
+        for (unsigned int a = 0; (a < strlen(key.c_str())) && isend;a++)
+        {
             if(i<strlen(res.c_str()))
             {
                 res[i] = res[i]+key[a]+keydigit;
@@ -216,7 +218,7 @@ void crypt (std::string &res, const std::string key)  //updated text encryption 
     trash.clear();
 }
 
-void decrypt (std::string &res, const std::string key) //decryption is performed in the reverse order of encryption
+void decrypt (std::string &res, const std::string key)
 {
     int keydigit = strlen(res.c_str()) % 4, isend = 1;;
     std::string itog;
@@ -245,205 +247,142 @@ void decrypt (std::string &res, const std::string key) //decryption is performed
     itog.clear();
 }
 
-void shifr (char *res, const char *key)
+void parcer(std:: string buffer, std::string &name, std::string &pwd, std::string &res)
 {
-    int reslen = 0, keylen = 0, keydigit = 0, trashlen = 0, itoglen = 0;
-    char trash[100000]="", itog[100000]="";
-    while(res[reslen]) reslen++;
-    for(int i = 0; i <= (reslen*2); i++){
-        trash[i] = rand() % 127;
-        if(isalnum(trash[i]) == 0) {
-            trash[i] = 63;
-        }
-        i++;
-        trash[i] = rand() % 127;
-        if(isalnum(trash[i]) == 0) {
-            trash[i] = 63;
-        }
-    }
-    trashlen = 0;
-    while(trash[trashlen]) trashlen++;
-    for(int i = 0, a = 0, j = 0; a < reslen; ){
-        itog[i++] = trash[j++];
-        itog[i++] = trash[j++];
-        itog[i++] = res[a++];
-    }
-    while(itog[itoglen]) itoglen++;
-    for(int i = 0; i < reslen; i++) res[i] = 0;
-    for(int i = 0; i < itoglen; i++) res[i] = itog[i];
-    while(key[keylen]) keylen++;
-    reslen = 0;
-    while(res[reslen]) reslen++;
-    keydigit = reslen % 4;
-    for (int i = 0; i < reslen;){
-        for (int a = 0; a < keylen;a++){
-            if(i<reslen){
-                res[i] = res[i]+key[a]+keydigit;
-                i++;
+    if(!buffer.empty())
+    {
+       int isname = 1, ispwd = 1, istext = 1;
+       unsigned int cur = 0;
+       std::string text;
+        for(unsigned int i = 0; i < strlen(buffer.c_str());)
+        {
+            if(buffer[i]=='\n' && isname)
+            {
+                for(unsigned int a = 0; a < i; a++)
+                {
+                    name += buffer[cur++];
+                }
+                isname = 0, cur++, i++;
             }
-            else{
-                break;
+            else if(buffer[i]=='\n' && ispwd)
+            {
+                for(unsigned int a = 0; a < (i-strlen(name.c_str())-1); a++)
+                {
+                    pwd += buffer[cur++];
+                }
+                ispwd = 0, cur++,i++;
+            }
+            else if(buffer[i]=='\n' && istext)
+            {
+                for(unsigned int a = 0; a < (i-(strlen(name.c_str())+strlen(pwd.c_str()))-2); a++)
+                {
+                    text += buffer[cur++];
+                }
+                istext = 0, i++;
+            }
+            else
+            {
+              i++;
             }
         }
+        res = name + ':' + text + '\n';
     }
 }
 
-void deshifr (char *res, const char *key)
+int valid(std::string name, std::string pwd)
 {
-    int reslen = 0, keylen = 0, keydigit = 0, itoglen = 0;
-    char itog[100000]="";
-    while(res[reslen]) reslen++;
-    keydigit = reslen % 4;
-    while(key[keylen]) keylen++;
-    for (int i = 0; i < reslen;){
-        for (int a = 0; a < keylen;a++){
-            if(i<reslen){
-                res[i] = res[i]-key[a]-keydigit;
-                i++;
-            }
-            else{
-                break;
-            }
-        }
-    }
-    for(int i = 0, a = 2; i < reslen; i++){
-        itog[i] = res[a];
-        a += 3;
-    }
-    while(itog[itoglen]) itoglen++;
-    for(int i = 0; i < reslen; i++) res[i] = 0;
-    for(int i = 0; i < itoglen; i++) res[i] = itog[i];
-}
-
-int valid(char *name, char *pwd)
-{
-    std::string needString =  std::string(name) + std::string(pwd);
-    std::string summHash = sha512(needString);
+    std::string summHash = sha512(name+pwd);
     std::string string;
     bool isHave = 0;
-    std::ifstream ifstream1("pwd.txt");
-    while( std::getline( ifstream1, string ) )
-        if( string == summHash ){
+    std::ifstream read("pwd.txt", std::ifstream::in);
+    while(std::getline(read, string))
+    {
+        if(string == summHash && isHave == 0)
+        {
             isHave = 1;
-            break;
         }
-    ifstream1.close();
-    return isHave;
-}
-
-
-char parce(char *buffer, char *name, char *pwd, char *text, char *res)
-{
-    int g = 0, c = 0, namelen = 0, textlen = 0, count = 0, parce = 0;
-    if(buffer[0]!='\0'){
-        parce = 1;
-        while(buffer[g] != '\n') name[c++] = buffer[g++];
-        name[c]='\0';
-        g++;
-        c=0;
-        while(buffer[g] != '\n')pwd[c++] = buffer[g++];
-        pwd[c]='\0';
-        g++;
-        c=0;
-        while(buffer[g])text[c++] = buffer[g++];
-        text[c]='\0';
-        while(name[namelen]) namelen++;
-        while(text[textlen]) textlen++;
-        for(int i = 0; i < namelen; i++, count++) res[count] = name[count];
-        res[count++] = ':';
-        for(int i = 0; i < textlen; i++, count++) res[count] = text[i];
     }
-    return parce;
+    read.close();
+    return isHave;
 }
 
 void SendMessageToClient(struct my_struct *condata)
 {
     int ID = condata->ID;
-    char accden[1024] = "access denied, push enter to continue\n";
+    std::string accden = "access denied, push enter to continue\n";
+    crypt(accden,key);
+    char accdenchar[strlen(accden.c_str())];
+    for(unsigned int i = 0; i < strlen(accden.c_str());i++) accdenchar[i] = accden[i];
     char time[40] = "";
-    for ( ; ; Sleep(750))
+    for ( ; ; Sleep(sleeptime))
     {
         std::ofstream logs("logs.txt", std::ios_base::app);
         logs.close();
-        char buffer[50000] = "";
-        for (int clear666 = 0; buffer[clear666] != 0; clear666++) buffer[clear666] = '\0';
-        if (recv(condata->Connections[ID], buffer, sizeof(buffer), 0)){
-            char res[50000] = "";
-            char pwd[1024] = "";
-            char name[1024] = "";
-            char text[50000] = "";
-            deshifr(buffer,key);
-            parce(buffer, name, pwd, text, res);
-            if((valid(name,pwd) == 0) && connect(condata->Connections[ID],0,0) != SOCKET_ERROR){
+        char buffer[buffersize] ="";
+        if (recv(condata->Connections[ID], buffer, sizeof(buffer), 0))
+        {
+            std::string name,pwd,res, buff = std::string(buffer);
+            decrypt(buff,key);
+            parcer(buff, name, pwd, res);
+            if((valid(name,pwd) == 0) && connect(condata->Connections[ID],0,0) != SOCKET_ERROR)
+            {
                 logs.open("logs.txt", std::ios_base::app);
                 settime(time);
                 logs << time;
                 logs << ": client id: " << ID << " : authorization attempt with name: " << name <<" <-- NO VALID!!!\n";
                 logs.close();
-                shifr(accden,key);
-                send(condata->Connections[ID],accden, sizeof(accden), 0);
-                deshifr(accden,key);
+                send(condata->Connections[ID],accdenchar, sizeof(accdenchar), 0);
                 shutdown(condata->Connections[ID],2);
                 closesocket(condata->Connections[ID]);
-                memset(buffer, 0, sizeof(buffer));
-                memset(res, 0, sizeof(res));
-                memset(pwd, 0, sizeof(pwd));
-                memset(name, 0, sizeof(name));
-                memset(text, 0, sizeof(text));
                 break;
             }
-            else if(res[0] != '\0'){
+            else if(!res.empty())
+            {
                 logs.open("logs.txt", std::ios_base::app);
                 settime(time);
                 logs << time;
                 logs << ": client id: " << ID << ":" << res;
                 logs.close();
-                shifr(res,key);
+                crypt(res,key);
+                char reschar[strlen(res.c_str())];
+                for(unsigned int i = 0; i < strlen(res.c_str());i++) reschar[i] = res[i];
                 for (int i = 0; i < condata->ClientCount; i++)
                 {
-                     if (i!=ID){
-                          send(condata->Connections[i], res, sizeof(res), 0);
+                    if (i!=ID)
+                    {
+                        send(condata->Connections[i], reschar, sizeof(reschar), 0);
                     }
                 }
             }
-            else {
+            else
+            {
                 logs.open("logs.txt", std::ios_base::app);
                 settime(time);
                 logs << time;
                 logs << ": client id: " << ID << " <- DISCONNECT!\n";
                 logs.close();
-                shifr(accden,key);
-                send(condata->Connections[ID],accden, sizeof(accden), 0);
-                deshifr(accden,key);
                 shutdown(condata->Connections[ID],2);
                 closesocket(condata->Connections[ID]);
-                memset(buffer, 0, sizeof(buffer));
-                memset(res, 0, sizeof(res));
-                memset(pwd, 0, sizeof(pwd));
-                memset(name, 0, sizeof(name));
-                memset(text, 0, sizeof(text));
                 break;
             }
-            memset(buffer, 0, sizeof(buffer));
-            memset(res, 0, sizeof(res));
-            memset(pwd, 0, sizeof(pwd));
-            memset(name, 0, sizeof(name));
-            memset(text, 0, sizeof(text));
         }
-        else {
+        else
+        {
             shutdown(condata->Connections[ID],2);
             closesocket(condata->Connections[ID]);
-            memset(buffer, 0, sizeof(buffer));
             break;
         }
     }
 }
 
+
+
+
 int main()
 {
     struct my_struct condata;
-    char accden[1024] = "access denied, push enter to continue\n";
-    shifr(accden, key);
+    std::string accden = "access denied, push enter to continue\n";
+    crypt(accden,key);
     SOCKET Connect;
     SOCKET Listen;
     char time[40] = "";
@@ -483,8 +422,10 @@ int main()
     logs << time;
     logs << ": Start server...\n";
     logs.close();
-    char m_connect[] = "online\n\n";
-    shifr(m_connect,key);
+    std::string m_connect = "online\n\n";
+    crypt(m_connect,key);
+    char m_connectchar[strlen(m_connect.c_str())];
+    for(unsigned int i = 0; i < strlen(m_connect.c_str());i++) m_connectchar[i] = m_connect[i];
     CreateThread(0,0,(LPTHREAD_START_ROUTINE)reguser,0,0,0);
     for(;;Sleep(75)) {
         if((Connect = accept(Listen,0,0)) != INVALID_SOCKET) {
@@ -494,33 +435,34 @@ int main()
             logs << ": Client id: " << condata.ClientCount << " connect...\n";
             logs.close();
             condata.Connections[condata.ClientCount] = Connect;
-            char buffer[50000], res[50000], pwd[1024], name[1024], text[50000];
+            char buffer[buffersize];
+            std::string name,pwd,res;
             recv(condata.Connections[condata.ClientCount], buffer, sizeof(buffer), 0);
-            deshifr(buffer,key);
-            parce(buffer, name, pwd, text, res);
+            std::string buff = std::string(buffer);
+            decrypt(buff,key);
+            parcer(buff, name, pwd, res);
             if(valid(name,pwd) == 0){
                 logs.open("logs.txt", std::ios_base::app);
                 settime(time);
                 logs << time;
                 logs << ": client id: " << condata.ClientCount << " : authorization attempt with name: " << name <<" <-- NO VALID!!!\n";
                 logs.close();
-                send(condata.Connections[condata.ClientCount],accden, sizeof(accden), 0);
+                char accdenchar[strlen(accden.c_str())];
+                for(unsigned int i = 0; i < strlen(accden.c_str());i++) accdenchar[i] = accden[i];
+                send(condata.Connections[condata.ClientCount],accdenchar, sizeof(accdenchar), 0);
                 shutdown(condata.Connections[condata.ClientCount],2);
                 closesocket(condata.Connections[condata.ClientCount]);
             }
             else {
-                shifr(res,key);
-                for (int i = 0; i < condata.ClientCount; i++) send(condata.Connections[i], res, sizeof(res), 0);
-                send(condata.Connections[condata.ClientCount],m_connect,strlen(m_connect),0);
+                crypt(res,key);
+                char reschar[strlen(res.c_str())];
+                for(unsigned int i = 0; i < strlen(res.c_str());i++) reschar[i] = res[i];
+                for (int i = 0; i < condata.ClientCount; i++) send(condata.Connections[i], reschar, sizeof(reschar), 0);
+                send(condata.Connections[condata.ClientCount],m_connectchar,sizeof(m_connectchar),0);
                 condata.ID = condata.ClientCount;
                 (condata.ClientCount)++;
                 CreateThread(0,0,(LPTHREAD_START_ROUTINE)SendMessageToClient,(LPVOID)&condata,0,0);
             }
-            memset(buffer, 0, sizeof(buffer));
-            memset(res, 0, sizeof(res));
-            memset(pwd, 0, sizeof(pwd));
-            memset(name, 0, sizeof(name));
-            memset(text, 0, sizeof(text));
         }
     }
     return 1;
